3a3f66744ffefdf9413028bed4239e19dd9ab19e
Juan Carlos Pazmino <juankpro@hotmail.com>
2012-06-12 15:51:32 -0500

    Added a bat file to ease compiling from PC

---
+++ bin/package.bat
@@ -0,0 +1,1 @@
+adt -package -target ane LocalNotificationLib.ane extension.xml -swc LocalNotificationLib.swc -platform Android-ARM -C android . -platform iPhone-ARM -C iphone . -platform default -C default library.swf
