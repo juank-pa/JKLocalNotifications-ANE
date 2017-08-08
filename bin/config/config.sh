f0f32084b3f85d8e865a41a0ec69a394ec889b3e
Juan Carlos Pazmiño <juankpro@outlook.com>
2017-08-08 08:38:49 -0500

    Add matrix for ios11

--- bin/config/config.sh
+++ bin/config/config.sh
@@ -1,3 +1,5 @@
+CONFIG_SCRIPT_PATH="$(dirname "$0")"
+
 # path to YOUR Android SDK
 SDK_PATH="$HOME/Developer/AIR_SDK"
 
@@ -8,19 +10,19 @@ ACOMPC="$SDK_PATH/bin/acompc"
 ADT="$SDK_PATH/bin/adt"
 
 # AS3 lib folder
-LIB_FOLDER=./../src/AS
+LIB_FOLDER=$CONFIG_SCRIPT_PATH/../../src/AS
 AIR_VERSION=26
 
 # ANDROID
 # path to android sdk
 AIR_ANDROID_SDK_HOME="$HOME/android-sdk-macosx"
 # native Eclipse project folder
-NATIVE_ANDROID_FOLDER=./../src/Eclipse
+NATIVE_ANDROID_FOLDER=$CONFIG_SCRIPT_PATH/../../src/Eclipse
 JAR_NAME=localnotification.jar
 
 # IOS
 # native XCode project folder
-NATIVE_XCODE_FOLDER=./../src/XCode/LocalNotificationLib
+NATIVE_XCODE_FOLDER=$CONFIG_SCRIPT_PATH/../../src/XCode/LocalNotificationLib
 IOS_LIB_NAME=LocalNotificationLib
 # the "name" must be the name of your physical device as listed in the XCode organizer.
 # your device needs to be connected in order to compile the library.
@@ -34,6 +36,6 @@ CONFIGURATION=Release
 ANE_NAME=LocalNotificationLib
 
 # helpers
-TEMP_PATH=./tmp
-CONFIG_PATH=./config
-RESULT_PATH=./ext
+TEMP_PATH=$CONFIG_SCRIPT_PATH/../tmp
+CONFIG_PATH=$CONFIG_SCRIPT_PATH/../config
+RESULT_PATH=$CONFIG_SCRIPT_PATH/../ext
